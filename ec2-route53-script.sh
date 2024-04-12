7b98395e2384582ee5c2a637329bbde4ed03c7c2
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2024-04-12 12:05:01 +0530

    automate ec2-route53

--- ec2-route53-script.sh
+++ ec2-route53-script.sh
@@ -13,22 +13,19 @@
 # --tags Key=Name,Value=Script
 
 CREATE_EC2(){
-    aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --count 1 --instance-type $2 
---key-name nv_keypair --security-group-ids sg-0ad71420a0b2e2f78 --subnet-id subnet-08a8ac34932166a4b
---tags Key=Name,Value=$1
+    aws ec2 run-instances --image-id ami-0f3c7d07486cad139  --instance-type $2 \
+--key-name nv_keypair --security-group-ids sg-0ad71420a0b2e2f78 --subnet-id subnet-08a8ac34932166a4b \
+--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]"
 }
+INSTANCE=("mongodb" "mysql" "redis" "rabbiMQ" "web" "user" "catalogue" "payment" "dispatch" "shipping")
 
-# INSTANCE=("mongodb" "mysql" "redis" "rabbiMQ" "web" "user" "catalogue" "payment" "dispatch" "shipping")
-
-# for i in "${INSTANCE[@]}"
-# do
-#     if [ $i == "mongodb" ] || [ $i == "shipping" ] || [ $i == "mysql" ];then 
-#         CREATE_EC2 $i "t3.instances"
-#     else
-#         CREATE_EC2 $i "t2.micro"
-#     fi
-# done 
+for i in "${INSTANCE[@]}"
+do
+    if [ $i == "mongodb" ] || [ $i == "shipping" ] || [ $i == "mysql" ];then 
+        CREATE_EC2 $i "t3.instances"
+    else
+        CREATE_EC2 $i "t2.micro"
+    fi
+done 
 
 # aws ec2 describe-instances --instance-ids <EC2_ID> --query 'Reservations[0].Instances[0].{"PrivateIP":PrivateIpAddress,"PublicIP":PublicIpAddress}'
-
-CREATE_EC2 "web" "t2.micro"
